b8f065622961a7f67a9ab6b71577a2a202630099
Anne Karoline Rodrigues <62261535+annie-bot@users.noreply.github.com>
2021-09-20 18:39:54 -0300

    Add files via upload

---
+++ config-server/src/test/java/br/com/fornecedor/abella/arquitetura/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fornecedor.abella.arquitetura.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
